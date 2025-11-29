4e18ad153542c487347512bc927dc14904fa1127
kbgarcia8 <kbgarcia8@gmail.com>
2025-11-29 23:22:15 +0800

    published 1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
-import GlobalStyle from './styles/globalStyles.js';
-import { createRoot } from 'react-dom/client'
 import { ThemeContextProvider } from '@context/ThemeContext';
 import Button from '@components/atoms/Button/Button';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeContextProvider>
-      <GlobalStyle />
       <Button onClick={() => alert("Button clicked!")}>Click Me</Button>
     </ThemeContextProvider>
   </StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -28,11 +28,17 @@ export default defineConfig({
       fileName: (format) => `react-dynamic-form.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'styled-components'], // ? Prevents Vite from bundling React into your library. Why? Consumers already have React installed. Bundling React can cause duplicate React errors.
+      external: [
+        'react', 
+        'react-dom', 
+        'react/jsx-runtime',
+        'styled-components'
+      ], // ? Prevents Vite from bundling React into your library. Why? Consumers already have React installed. Bundling React can cause duplicate React errors.
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'React',
           'styled-components': 'styled'
         },
       },
